bd48f105e4de875b8c44aed300a5491bab386b27
David Cadena <68673038+Dav1dCad3na@users.noreply.github.com>
2025-04-15 07:21:37 -0600

    Updated Creator text

--- CustomerManager.py
+++ CustomerManager.py
@@ -1,5 +1,5 @@
 # Needed to make some changes for functionality with UI. - Brett
-
+# Created By David Cadena
 class CustomerManager:
     def __init__(self):
         # Initialize instance variables
