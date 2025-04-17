0372a0a8547a4fab1c15bf9594b0dc52d573b1a3
ConnorYasin <ConnorYasin@gmail.com>
2025-04-17 13:45:12 -0600

    Deleted the useless classes

--- Category.py
+++
@@ -1,8 +0,0 @@
-#Connor Yasinski
-
-#Might Be an irrelevent class
-class Category: 
-    def __init__(self, categoryNumber, description):
-        self.categoryNumber = categoryNumber
-        self.description = description
-        
